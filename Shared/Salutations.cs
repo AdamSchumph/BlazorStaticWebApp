91b749fe8692dbd5de5e2d1e9246677fb699011c
Adam Schumph <adamschumph@gmail.com>
2021-06-03 22:35:20 -0700

    Separate salutationsMessage and wire up front end

---
+++ Shared/Salutations.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlazorStaticWebApp.Shared
+{
+
+    public class SalutationsResponse
+    {
+        public string Message {get;set;}
+    }
+}
